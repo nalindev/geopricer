6a72f6151b3e87f114fe3a4e19b003131bbdfdbb
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2024-10-02 20:45:13 +0530

    cache workfile

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
           <li className="mb-2">
             Nalin Nishant
           </li>
-          <li>so, v1</li>
+          <li>so, v2 Start Deploy</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
